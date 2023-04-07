bba6f80f6721af87dd5e9e824e87c390bf4c29d1
Isiii555 <islam.salamzade.191@gmail.com>
2023-04-07 15:32:50 +0400

    Proxy

---
+++ ProxyPattern/ProxyPattern/Concretes/Bank.cs
@@ -0,0 +1,17 @@
+﻿using ProxyPattern.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern.Concretes
+{
+    internal class Bank : IBank
+    {
+        public void SendMoney(double amount)
+        {
+            Console.WriteLine($"Transaction successfully finished : {amount}");
+        }
+    }
+}
